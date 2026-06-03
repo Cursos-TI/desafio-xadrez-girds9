1b9b436b7a783c1529646bb8c5efa2771f8d0415
Giovanna Rodrigues <contatogirds@gmail.com>
2026-06-03 03:21:55 +0000

    Desafio Xadrez - Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -10,37 +10,58 @@ int main() {
     int bispo = 1;
     int rainha = 1;
     int torre = 1;
+    int movimento = 1;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
     
+    printf("Movimentos do Bispo\n\n");
     while (bispo <= 5)
     {
         printf("Cima Direita\n");
         bispo++;
     }
 
+    printf("\n");
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
     
+    printf("Movimentos da Torre\n\n");
     do
     {
         printf("Direita\n");
         torre++;    
     } while (torre <= 5);
     
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    
+    printf("Movimentos da Rainha\n\n");
     for (int rainha = 1; rainha <= 8; rainha++)
     {
         printf("Esquerda\n");
     }
     
+    printf("\n");
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+   
+   printf("Movimentos do Cavalo\n\n");
+    while (movimento--)
+    {
+        for (int movimento = 0; movimento < 2; movimento++)
+        {
+            printf("Baixo\n");
+        }
+        printf("Esquerda\n");
+    }
+    
+    printf("\n");
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
